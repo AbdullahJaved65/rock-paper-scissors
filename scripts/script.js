632d35ad69ac31a19f595225aed020749cbebf17
Abdullah Javed <abdullahjaved65@yahoo.com>
2023-08-09 15:50:18 +0500

    Fix the logical errors

--- scripts/script.js
+++ scripts/script.js
@@ -3,59 +3,74 @@ function getComputerChoice() {
   return computerChoices[randomElement];
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (
-    playerSelection === "rock" ||
-    playerSelection === "scissor" ||
-    (playerSelection === "paper" && playerSelection != null)
-  ) {
-    if (
-      (playerSelection === "rock" && computerSelection === "rock") ||
-      (playerSelection === "paper" && computerSelection === "paper") ||
-      (playerSelection === "scissor" && computerSelection === "scissor")
-    ) {
-      console.log("It is a draw!");
-    } else if (playerSelection === "rock" && computerSelection === "scissor") {
-      console.log("You've won!");
-      playerScore++;
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-      console.log("Looks like you've lost this one pal!");
-      computerScore++;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-      console.log("You've won!");
-      playerScore++;
-    } else if (playerSelection === "paper" && computerSelection === "scissor") {
-      console.log("Looks like you've lost this one pal!");
-      computerScore++;
-    } else if (playerSelection === "scissor" && computerSelection === "rock") {
-      console.log("Looks like you've lost this one pal!");
-      computerScore++;
-    } else if (playerSelection === "scissor" && computerSelection === "paper") {
-      console.log("You've won!");
-      playerScore++;
-    }
-  } else {
-    console.log("Please make a correct selection.");
-  }
-}
+// function playRound(playerSelection, computerSelection) {}
 
 function game() {
   for (let gameCount = 1; gameCount <= 5; gameCount++) {
-    playRound(playerSelection, computerSelection);
-    if (playerScore === computerScore) {
-      console.log("You're both tied up!");
-    } else if (playerScore > computerScore) {
-      console.log("You won!");
-    } else if (playerScore < computerScore) {
-      console.log("Looks like you've lost this one pal!");
+    let playerSelection = prompt("Which one will it be? ").toLowerCase();
+    const computerSelection = getComputerChoice();
+
+    if (
+      playerSelection === "rock" ||
+      playerSelection === "scissor" ||
+      (playerSelection === "paper" && playerSelection != null)
+    ) {
+      if (
+        (playerSelection === "rock" && computerSelection === "rock") ||
+        (playerSelection === "paper" && computerSelection === "paper") ||
+        (playerSelection === "scissor" && computerSelection === "scissor")
+      ) {
+        console.log("It is a draw!");
+      } else if (
+        playerSelection === "rock" &&
+        computerSelection === "scissor"
+      ) {
+        console.log("You've won!");
+        playerScore++;
+      } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("Looks like you've lost this one pal!");
+        computerScore++;
+      } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("You've won!");
+        playerScore++;
+      } else if (
+        playerSelection === "paper" &&
+        computerSelection === "scissor"
+      ) {
+        console.log("Looks like you've lost this one pal!");
+        computerScore++;
+      } else if (
+        playerSelection === "scissor" &&
+        computerSelection === "rock"
+      ) {
+        console.log("Looks like you've lost this one pal!");
+        computerScore++;
+      } else if (
+        playerSelection === "scissor" &&
+        computerSelection === "paper"
+      ) {
+        console.log("You've won!");
+        playerScore++;
+      }
+      console.log(playerScore);
+      console.log(computerScore);
+    } else {
+      console.log("Please make a correct selection.");
     }
+  }
+  if (playerScore === computerScore) {
+    console.log("You're both tied up!");
+  } else if (playerScore > computerScore) {
+    console.log("You won!");
+  } else if (playerScore < computerScore) {
+    console.log("Looks like you've lost this one pal!");
   }
 }
 
 const computerChoices = ["rock", "paper", "scissor"];
-const computerSelection = getComputerChoice();
-let playerSelection = prompt("Which one will it be? ").toLowerCase();
-let playerScore,
-  computerScore = 0;
+
+// let playerSelection = "rock";
+let playerScore = 0;
+let computerScore = 0;
 
 game();
